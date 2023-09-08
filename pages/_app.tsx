676500ba47f52555ab87ae19651ff50ad49275df
daveclinton <clintondavid46@gmail.com>
2023-09-08 15:59:26 +0300

    fix: remove footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { Header } from "@/components/Header";
-import { Footer } from "@/components/Social";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
