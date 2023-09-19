9d7a403ee6b388ef0e6cbccb56758b37c61d7be2
daveclinton <clintondavid46@gmail.com>
2023-09-19 12:53:39 -0400

    feat: Add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         href="#main"
         className="fixed p-2 top-0 left-0 -translate-y-full focus:translate-y-0"
       >
-        Skip to main content
+        Skip to main contents
       </a>
       <div className="flex flex-col max-w-3xl mx-auto min-h-full px-4">
         <Header />
